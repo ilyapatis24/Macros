b648184782a771dd50a77cc8a848259ad8a03602
ilyapatis24 <ilyapatis24@mail.ru>
2023-12-06 23:46:25 +0300

    initial commit

---
+++ 01/main.cpp
@@ -0,0 +1,36 @@
+﻿#define MODE 1
+
+#ifdef MODE
+
+#if MODE == 1
+int add(int a, int b)
+{
+	return a + b;
+}
+#endif
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	system("chcp 1251");
+#if MODE == 0
+	cout << "Работаю в режиме тренировки\n";
+#elif MODE == 1
+	cout << "Работаю в боевом режиме\n";
+	int a, b;
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	cout << "Результат сложения: " << add(a, b) << endl;
+#else
+	cout << "Неизвестный режим. Завершение работы\n";
+#endif
+	return 0;
+}
+
+#else
+#error "it is necessary to define MODE"
+#endif
