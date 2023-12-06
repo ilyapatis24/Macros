02c45651a1796999e5f47049ba99f832475a0d17
ilyapatis24 <ilyapatis24@mail.ru>
2023-12-06 23:57:42 +0300

    second mission Macros

---
+++ 02/main.cpp
@@ -0,0 +1,17 @@
+﻿#define SUB(a, b) ((a)-(b))
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	system("chcp 1251");
+
+	int a = 6, b = 5, c = 2;
+	cout << SUB(a, b) << endl;
+	cout << SUB(a, b) * c << endl;
+	cout << SUB(a, b + c) * c << endl;
+	return 0;
+}
